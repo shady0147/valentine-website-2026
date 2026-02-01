f9fadfce28370051dd847f6dbd1b905a71478284
shady0147 <36852831+shady0147@users.noreply.github.com>
2026-02-01 22:54:18 +0200

    Update celebration emojis in config.js
    Added a unicorn emoji to the celebration message.

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ const CONFIG = {
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
         message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        emojis: "🎁💖🤗💝💋❤️💕🦄"  // These will bounce around
     },
 
     // Color scheme for the website
